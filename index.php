ac481143b536973fd55c3fda9410561e9cac23b7
kyubisma <kyuu69x@gmail.com>
2018-10-20 10:01:46 +0700

    a
    add index

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css.css">
+	<title>project</title>
+</head>
+<body style="height: 2000px;">
+	<div class="header">
+		<a href="#">Data Diri</a>
+		<a href="#">Raport</a>
+		<a href="#">Absensi</a>
+		<a href="#">Home</a>
+	</div>
+	<div class="shadow">a</div>
+	<div class="body">
+		<img class="sampul" src="bg12.jpg" >
+		<img class="profil" src="pp1.jpg">
+		<div class="nama">
+			<style type="text/css">
+				label{
+					margin-top: 20px;
+					position: absolute;
+					width: 1000px
+				}
+			</style>
+				<label class="">Bhisma Ajuni Sutarna</label>
+				<br>
+				<label class="">12RPL2</label>
+		</div>
+	</div>
+</body>
+</html>
